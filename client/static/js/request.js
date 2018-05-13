30c1139e83dcd4fa7cff471efdb0f74eeeb0417d
puzhiyuan <abortion2012@gmail.com>
2018-05-13 14:10:21 +0800

    update: cors跨域

--- client/static/js/request.js
+++ client/static/js/request.js
@@ -13,6 +13,7 @@ describe("cross origin request", function() {
       // 只能处理GET请求，需要被调用端的代码修改
       $.ajax({
         url: remoteBase + "/jsonp",
+        // method: 'POST', // 无效 JSONP 不支持
         dataType: "jsonp", // 从服务器返回你期望的数据类型
         // jsonp: "wakakakaka", // 与后端约定的qs取函数的键
         // jsonpCallback: "fooo", // 与后端约定的函数名字
@@ -21,6 +22,73 @@ describe("cross origin request", function() {
         // jquery 生成一个全局函数 指向success这个方法
         success: function(data) {
           expect(data).toEqual({ name: "fooBar" });
+          done();
+        }
+      });
+    },
+    2000
+  );
+
+  // Failed to load http://localhost:30001/cors: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:30000' is therefore not allowed access.
+  // 直接调用下面的代码会报如上错误，在http-response-header中没有指定Access-Control-Allow-Origin 需在被调用端处理
+  it(
+    "cors GET request",
+    function(done) {
+      $.ajax({
+        url: remoteBase + "/cors",
+        success: function(data) {
+          expect(data).toEqual({ name: "fooBar" });
+          done();
+        }
+      });
+    },
+    2000
+  );
+
+  it(
+    "cors POST request",
+    function(done) {
+      // 这里的content-type
+      // Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+      $.ajax({
+        url: remoteBase + "/cors",
+        method: "POST",
+        data: { name: 123 },
+        success: function(data) {
+          expect(data).toEqual({ name: "fooBar" });
+          done();
+        }
+      });
+    },
+    2000
+  );
+
+  // 简单请求
+  // method: GET HEAD POST
+  // Content-Type: text/plain multipart/form-data application/x-www-form-urlencoded
+  // requestHeader 没有自定义请求头
+
+  // 非简单请求
+  // method: put delete
+  // Content-Type: application/json
+  // 带自定义请求头
+
+  // 非简单请求  preflight request
+
+  it(
+    "POST json request",
+    function(done) {
+      var postData = JSON.stringify({ name: 123 });
+      // 这里的content-type
+      // Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+      $.ajax({
+        url: remoteBase + "/postJson",
+        method: "POST",
+        data: postData,
+        dataType: "JSON",
+        contentType: "application/json;chartset=UTF-8",
+        success: function(data) {
+          expect(data).toEqual(postData);
           done();
         }
       });
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var querystring = require("querystring");
 var express = require("express");
 var app = express();
 var server = http.createServer(app);
@@ -15,12 +16,57 @@ server.listen(port, function() {
 // static resource route
 app.use(express.static(__dirname + "/static"));
 
+app.use(function(req, res, next) {
+  if (req.method === "POST") {
+    var body = "";
+    req.on("data", function(chunk) {
+      body += chunk;
+    });
+
+    req.on("end", function() {
+      req.body = body;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
 app.get("/", function(req, res) {
   res.send("Server side is working");
 });
 
-app.get("/jsonp", function(req, res) {
+app.all("/jsonp", function(req, res) {
   var callbackName = req.query.callback;
   var resText = callbackName + "(" + JSON.stringify(data) + ")";
   res.send(resText);
+});
+
+app.all("/cors", function(req, res) {
+  // Access-Control-Allow-Origin, 表示允许跨域使用的域名
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:30000");
+  // Access-Control-Allow-Origin, 表示允许使用的http请求类型
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.send(data);
+});
+
+app.all("/postJson", function(req, res) {
+  // Acess-Control-Allow-Origin, Access-Control-Allow-Methods 可以使用通配符，也可以使用从req中读取的值
+  var origin = req.get("origin");
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", req.method);
+  // 设置 OPTIONS 缓存时间
+  res.setHeader("Access-Control-Max-Age", 3600);
+
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "*");
+
+  // Request header field Content-Type is not allowed by Access-Control-Allow-Headers in preflight response.
+  // xhr content-type 是json 非简单请求, 浏览器处理res报错
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // 非简单请求 处理预检请求
+  if (req.method == "OPTIONS") res.send(200);
+
+  res.json(req.body);
 });
